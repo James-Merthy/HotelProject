36d0ab708091717230ba542ca5f541559f52a3db
Mepho9 <babayev.r@outlook.com>
2022-07-15 14:12:16 +0200

    class  ... completed

--- src/main/java/model/Client.java
+++ src/main/java/model/Client.java
@@ -1,10 +1,49 @@
 package model;
 
-public class Client {
-    long id;
+import java.time.LocalDate;
 
-    public static void main(String[] args) {
-        System.out.println("sozoijfdzoi");
+public class Client {
+    private long clientID;
+    private String firstName;
+    private String lastName;
+    private LocalDate birthDate;
+
+    public Client(long id, String firstName, String lastName, LocalDate birthDate) {
+        this.clientID = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.birthDate = birthDate;
     }
 
+    public long getClientID() {
+        return clientID;
+    }
+
+    public void setClientID(long id) {
+        this.clientID = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
 }
